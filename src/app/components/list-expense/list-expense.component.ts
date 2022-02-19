722c54de23f4341e18d00bc57b45b29e81e3b5c2
Murshalin Ridoy <murshalin98@gmail.com>
2022-02-19 17:04:01 +0600

    add search functionality

--- src/app/components/list-expense/list-expense.component.ts
+++ src/app/components/list-expense/list-expense.component.ts
@@ -11,6 +11,10 @@ export class ListExpenseComponent implements OnInit {
 
   expenses: Expense[] = [];
 
+  filters = {
+    keyword: ''
+  }
+
   constructor(private _expenseService: ExpenseService) { }
 
   ngOnInit(): void {
@@ -28,8 +32,13 @@ export class ListExpenseComponent implements OnInit {
 
   listExpenses(){
     this._expenseService.getExpenses().subscribe(
-      data => this.expenses = data
+      data => this.expenses = this.filterExpenses(data)
     )
   }
 
+  filterExpenses(expenses: Expense[]){
+    return expenses.filter((e) => {
+      return e.expense.toLowerCase().includes(this.filters.keyword.toLowerCase());
+    })
+  }
 }
